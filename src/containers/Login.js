3219eb39e53cc599da1439a144f76a7235a7bb08
Chirag Agrawal <cagrawal@techracers.io>
2019-03-11 19:28:53 +0530

    created logon component

---
+++ src/containers/Login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  Form, Icon, Input, Button,
+} from 'antd';
+
+class Login extends React.Component {
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Item>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+const WrappedLogin = Form.create({ name: 'register' })(Login);
+export default WrappedLogin;
+
