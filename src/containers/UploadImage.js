72741ba211daa80c6bb899369578768f3d48f929
Chirag Agrawal <cagrawal@techracers.io>
2019-03-12 22:08:02 +0530

    added styling in the upload-image page

--- src/containers/UploadImage.js
+++ src/containers/UploadImage.js
@@ -11,24 +11,28 @@ class UploadImage extends React.Component {
   render() {
 
     const { Title } = Typography;
+
     return (
-      <Grid style={{ margin: '4px', backgroundColor: '#DCDCDC' }}>
-        <Row>
-          <Col span={12} style={{ textAlign: 'center', padding: '20px', border: '5px solid purple', height: '98vh' }}>
-            <Grid>
-              <Title level={3}>Upload Your Invoice</Title>
-              <Title level={4} strong>Max File Size: 5MB</Title>
-              <Upload >
-                <Button>
-                  <Icon type="upload" /> Click to Upload
-                </Button>
-              </Upload>
-            </Grid>
-          </Col>
-          <Col span={12} style={{ textAlign: 'center', padding: '20px', border: '5px solid purple', height: '98vh' }}>
-            <Grid>
-              <Title level={3}>Preview Image</Title>
-            </Grid>
+      <Grid style={{ paddingTop: 100, backgroundColor: '#f1f1f1', height: '100vh', }}>
+        <Row style={{ textAlign: 'center' }} >
+          <Col style={{ width: 800, height: '50vh', margin: '0 auto', boxShadow: '-2px 1px 23px #ccc' }}>
+            <Col span={12} style={{ textAlign: 'center', padding: '20px', height: '50vh', backgroundColor: '#fff', display: 'flex', borderTopLeftRadius: 5, borderBottomLeftRadius: 5, }}>
+              <Grid style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', flex: 100, }}>
+                <Title style={{ color: '#1DA57A', fontSize: 16, textTransform: 'uppercase', }}>Upload Your Invoice</Title>
+                <Upload>
+                  <Button style={{ height: 110, width: 126, fontSize: 14, }}>
+                    <Icon type="upload" style={{ display: 'block', fontSize: 20, }} />
+                    <Typography>Click to Upload</Typography>
+                  </Button>
+                  <Title style={{ fontSize: 13, marginTop: 15, fontWeight: 400, color: '#9c9c9cd9' }}>Max File Size: 5MB</Title>
+                </Upload>
+              </Grid>
+            </Col>
+            <Col span={12} style={{ textAlign: 'center', padding: '20px', borderLeft: '1px solid #eee ', height: '50vh', backgroundColor: '#fff', borderTopRightRadius: 5, borderBottomRightRadius: 5, display: 'flex', flex: 100, alignItems: 'center', justifyContent: 'center', }}>
+              <Grid>
+                <Title level={3} style={{ color: '#f1f1f1' }}>Preview Image</Title>
+              </Grid>
+            </Col>
           </Col>
         </Row>
       </Grid>
