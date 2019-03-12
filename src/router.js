f8e44f572b5307b267913d6b88ba72a151dec550
Chirag Agrawal <cagrawal@techracers.io>
2019-03-12 13:11:07 +0530

    added login page in the router file

--- src/router.js
+++ src/router.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Router, Route} from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import UploadImage from './containers/UploadImage';
+import createHistory from 'history/createBrowserHistory'
+import Login from './containers/Login'
 
-function AppRouter(){
-  return(
-    <Router>
-      <Route path="/upload_image" exact component={UploadImage}/>
+const history = createHistory();
+
+function AppRouter() {
+  return (
+    <Router history={history}>
+      <div>
+        <Route path="/" exact component={Login} />
+        <Route path="/upload_image" exact component={UploadImage} />
+      </div>
     </Router>
   )
 }
 
-export default AppRouter();
+export default AppRouter;
 
