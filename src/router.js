0bca1284fb507c17c72d9aa650e2642223f8cb21
Chirag Agrawal <cagrawal@techracers.io>
2019-03-13 19:33:53 +0530

    added an header and logout button in the router file

--- src/router.js
+++ src/router.js
@@ -3,19 +3,50 @@ import { Router, Route } from 'react-router-dom';
 import UploadImage from './containers/UploadImage';
 import createHistory from 'history/createBrowserHistory'
 import Login from './containers/Login'
+import { Layout } from 'antd';
+import { Button, Col } from 'antd';
 
 const history = createHistory();
 
+const LogOutButton = ({ history }) => {
+  const handleLogOut = () => {
+    localStorage.removeItem('authToken');
+    history.push('/');
+  }
+  if (localStorage.getItem('authToken')) {
+    return (
+      <Button
+        type="primary ant-btn-round"
+        style={{ align: 'middle' }} ii
+        onClick={handleLogOut}
+      >
+        Log Out
+      </Button>
+    )
+  } else {
+    return null
+  }
+}
+
 function AppRouter() {
+  const { Header } = Layout;
   return (
     <Router history={history}>
       <div>
+        <Layout>
+          <Header style={{ color: 'white', fontSize: '20px' }}>
+            <Col className="ant-col-xl-12">Vendors Dashboard</Col>
+            <Col style={{ textAlign: 'right' }} className="ant-col-xl-12">
+              <LogOutButton history={history} />
+            </Col>
+          </Header>
+        </Layout>
         <Route path="/" exact component={Login} />
         <Route path="/upload_image" exact component={UploadImage} />
       </div>
     </Router>
+
   )
 }
 
 export default AppRouter;
-
