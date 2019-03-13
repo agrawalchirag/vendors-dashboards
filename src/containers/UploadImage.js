c4e1ce5e8d5767918ca344fa3712b3665bfcac71
Chirag Agrawal <cagrawal@techracers.io>
2019-03-13 13:33:01 +0530

    added style object for styling in uploading_image component

--- src/containers/UploadImage.js
+++ src/containers/UploadImage.js
@@ -6,6 +6,77 @@ import { Button } from 'antd';
 import { Upload } from 'antd';
 import { Icon } from 'antd';
 
+const styles = {
+  gridMain: {
+    paddingTop: 100,
+    backgroundColor: '#f1f1f1',
+    height: '100vh',
+  },
+  gridSecond: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    flex: 100,
+  },
+  rowMain: {
+    textAlign: 'center',
+  },
+  columnMain: {
+    width: 800,
+    height: '50vh',
+    margin: '0 auto',
+    boxShadow: '-2px 1px 23px #ccc'
+  },
+  columnSecond: {
+    textAlign: 'center',
+    padding: '20px',
+    height: '50vh',
+    backgroundColor: '#fff',
+    display: 'flex',
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+  },
+  columnThird: {
+    textAlign: 'center',
+    padding: '20px',
+    borderLeft: '1px solid #eee ',
+    height: '50vh',
+    backgroundColor: '#fff',
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+    display: 'flex',
+    flex: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleMain: {
+    color: '#1DA57A',
+    fontSize: 16,
+    textTransform: 'uppercase',
+  },
+  titleSecond: {
+    fontSize: 13,
+    marginTop: 15,
+    fontWeight: 400,
+    color: '#9c9c9cd9',
+  },
+  buttonMain: {
+    height: 110,
+    width: 126,
+    fontSize: 14,
+  },
+  iconMain: {
+    display: 'block',
+    fontSize: 20,
+  },
+  imgMain: {
+    width: "100%"
+  },
+  titleThird: {
+    color: '#f1f1f1',
+  },
+};
+
 class UploadImage extends React.Component {
 
   state = {
@@ -18,41 +89,44 @@ class UploadImage extends React.Component {
 
     const { fileList } = this.state;
     const { Title } = Typography;
+
     return (
-      <Grid style={{ paddingTop: 100, backgroundColor: '#f1f1f1', height: '100vh', }}>
-        <Row style={{ textAlign: 'center' }} >
-          <Col style={{ width: 800, height: '50vh', margin: '0 auto', boxShadow: '-2px 1px 23px #ccc' }}>
-            <Col span={12} style={{ textAlign: 'center', padding: '20px', height: '50vh', backgroundColor: '#fff', display: 'flex', borderTopLeftRadius: 5, borderBottomLeftRadius: 5, }}>
-              <Grid style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', flex: 100, }}>
-                <Title style={{ color: '#1DA57A', fontSize: 16, textTransform: 'uppercase', }}>Upload Your Invoice</Title>
+      <Grid style={styles.gridMain}>
+        <Row style={styles.rowMain} >
+          <Col style={styles.columnMain}>
+            <Col span={12} style={styles.columnSecond}>
+              <Grid style={styles.gridSecond}>
+                <Title style={styles.titleMain}>Upload Your Invoice</Title>
                 <Upload
                   action="http://localhost:5000/invoice"
                   fileList={fileList}
                   name="invoice"
                   onChange={this.handleChange}
                 >
-                  {fileList.length ? null : <Button style={{ height: 110, width: 126, fontSize: 14, }}>
-                    <Icon type="upload" style={{ display: 'block', fontSize: 20, }} />
-                    <Typography>Click to Upload</Typography>
-                  </Button>
+                  {fileList.length
+                    ? null
+                    : <Button style={styles.buttonMain}>
+                      <Icon type="upload" style={styles.iconMain} />
+                      <Typography>Click to Upload</Typography>
+                    </Button>
                   }
-                  <Title style={{ fontSize: 13, marginTop: 15, fontWeight: 400, color: '#9c9c9cd9' }}>
+                  <Title style={styles.titleSecond}>
                     Max File Size: 5MB
                   </Title>
                 </Upload>
               </Grid>
             </Col>
-            <Col span={12} style={{ textAlign: 'center', padding: '20px', borderLeft: '1px solid #eee ', height: '50vh', backgroundColor: '#fff', borderTopRightRadius: 5, borderBottomRightRadius: 5, display: 'flex', flex: 100, alignItems: 'center', justifyContent: 'center', }}>
+            <Col span={12} style={styles.columnThird}>
               <Grid>
                 {fileList.length && fileList[0].response
-                  ? <img src={fileList[0].response.fileUrl} style={{ width: "100%" }} />
-                  : <Title level={3} style={{ color: '#f1f1f1' }}>Preview Image</Title>
+                  ? <img src={fileList[0].response.fileUrl} style={styles.imgMain} />
+                  : <Title level={3} style={styles.titleThird}>Preview Image</Title>
                 }
               </Grid>
             </Col>
           </Col>
         </Row>
-      </Grid>
+      </Grid >
     );
   }
 }
