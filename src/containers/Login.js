5ad921c78b05800993550c7dab66992008d703c3
Chirag Agrawal <cagrawal@techracers.io>
2019-03-13 12:11:11 +0530

    imported Axios and also ontegrated backend Api in the login page for username and password authentication

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,35 +4,52 @@ import {
 } from 'antd';
 import Grid from 'antd/lib/card/Grid';
 import { withRouter } from 'react-router-dom';
+import Axios from 'axios';
 
 class Login extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.history.push('/upload_image');
+    this.props.form.validateFields(async (err, values) => {
+      if (!err) {
+        try {
+          const { data } = await Axios.post('http://localhost:5000/signIn', values)
+          if (!data.result._id) {
+            throw new Error("Cannot find User")
+          }
+          this.props.history.push('/upload_image');
+        }
+        catch (e) {
+          console.error(e);
+        }
+      }
+    });
+
   }
 
   render() {
     const { getFieldDecorator } = this.props.form;
     const { Title } = Typography;
     return (
-      <Grid style={{ backgroundColor: '#f1f1f1', height:'100vh', padding:'13%' }}>
-        <Grid style={{ backgroundColor: 'white', padding: '20px', textAlign: 'center',width:400, margin:'0 auto',
-    alignItems: 'center', borderRadius:'5px', boxShadow:'-2px 1px 23px #ccc', }}>
-          <Title style={{ fontSize:24,marginTop:15, marginBottom:30,}}>Login Form</Title>
+      <Grid style={{ backgroundColor: '#f1f1f1', height: '100vh', padding: '13%' }}>
+        <Grid style={{
+          backgroundColor: 'white', padding: '20px', textAlign: 'center', width: 400, margin: '0 auto',
+          alignItems: 'center', borderRadius: '5px', boxShadow: '-2px 1px 23px #ccc',
+        }}>
+          <Title style={{ fontSize: 24, marginTop: 15, marginBottom: 30, }}>Login Form</Title>
           <Form onSubmit={this.handleSubmit}>
-            <Form.Item style={{ marginBottom:20,}}>
-              {getFieldDecorator('userName', {
+            <Form.Item style={{ marginBottom: 20, }}>
+              {getFieldDecorator('email', {
                 rules: [{ required: true, message: 'Please input your username!' }],
               })(
-                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} style={{height:45,}} type="email" placeholder="Username" />
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} style={{ height: 45, }} type="email" placeholder="Username" />
               )}
             </Form.Item>
-            <Form.Item style={{ marginBottom:30,}}>
+            <Form.Item style={{ marginBottom: 30, }}>
               {getFieldDecorator('password', {
                 rules: [{ required: true, message: 'Please input your Password!' }],
               })(
-                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} style={{height:45,}}  type="password" placeholder="Password" />
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} style={{ height: 45, }} type="password" placeholder="Password" />
               )}
             </Form.Item>
             <Form.Item>
