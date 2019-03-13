54a96c1f5f4333ec9657c532787f2db01ebe9fb8
Chirag Agrawal <cagrawal@techracers.io>
2019-03-13 12:59:52 +0530

    made a single style object for inline style

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,6 +6,42 @@ import Grid from 'antd/lib/card/Grid';
 import { withRouter } from 'react-router-dom';
 import Axios from 'axios';
 
+const styles = {
+  gridMain: {
+    backgroundColor: '#f1f1f1',
+    height: '100vh',
+    padding: '13%'
+  },
+  gridSecond: {
+    backgroundColor: 'white',
+    padding: '20px',
+    textAlign: 'center',
+    width: 400,
+    margin: '0 auto',
+    alignItems: 'center',
+    borderRadius: '5px',
+    boxShadow: '-2px 1px 23px #ccc',
+  },
+  titleMain: {
+    fontSize: 24,
+    marginTop: 15,
+    marginBottom: 30,
+  },
+  inputMain: {
+    height: 45,
+  },
+  itemMain: {
+    marginBottom: 30,
+  },
+  iconMain: {
+    color: 'rgba(0,0,0,.25)',
+  },
+  buttonMain: {
+    align: 'middle'
+  },
+};
+
+
 class Login extends React.Component {
 
   handleSubmit = (e) => {
@@ -31,31 +67,42 @@ class Login extends React.Component {
     const { getFieldDecorator } = this.props.form;
     const { Title } = Typography;
     return (
-      <Grid style={{ backgroundColor: '#f1f1f1', height: '100vh', padding: '13%' }}>
-        <Grid style={{
-          backgroundColor: 'white', padding: '20px', textAlign: 'center', width: 400, margin: '0 auto',
-          alignItems: 'center', borderRadius: '5px', boxShadow: '-2px 1px 23px #ccc',
-        }}>
-          <Title style={{ fontSize: 24, marginTop: 15, marginBottom: 30, }}>Login Form</Title>
+      <Grid style={styles.gridMain}>
+        <Grid style={styles.gridSecond}>
+          <Title style={styles.titleMain}>Login Form</Title>
           <Form onSubmit={this.handleSubmit}>
-            <Form.Item style={{ marginBottom: 20, }}>
+            <Form.Item style={styles.itemMain}>
               {getFieldDecorator('email', {
                 rules: [{ required: true, message: 'Please input your username!' }],
               })(
-                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} style={{ height: 45, }} type="email" placeholder="Username" />
+                <Input
+                  prefix={<Icon type="user" style={styles.iconMain} />}
+                  style={styles.inputMain}
+                  type="email"
+                  placeholder="Username"
+                />
               )}
             </Form.Item>
-            <Form.Item style={{ marginBottom: 30, }}>
+            <Form.Item style={styles.itemMain}>
               {getFieldDecorator('password', {
                 rules: [{ required: true, message: 'Please input your Password!' }],
               })(
-                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} style={{ height: 45, }} type="password" placeholder="Password" />
+                <Input
+                  prefix={<Icon type="lock" style={styles.iconMain} />}
+                  style={styles.inputMain}
+                  type="password"
+                  placeholder="Password"
+                />
               )}
             </Form.Item>
             <Form.Item>
-              <Button type="primary ant-btn-round" htmlType="submit" style={{ align: 'middle' }} >
+              <Button
+                type="primary ant-btn-round"
+                htmlType="submit"
+                style={styles.buttonMain}
+              >
                 Log in
-          </Button>
+              </Button>
             </Form.Item>
           </Form>
         </Grid>
