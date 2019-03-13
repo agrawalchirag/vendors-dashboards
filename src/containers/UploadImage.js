325b38f612c8f82f42dd5f3563db73178ea2b1d0
Chirag Agrawal <cagrawal@techracers.io>
2019-03-13 12:14:17 +0530

    added upload image and also preview image feature in the UploadImage component

--- src/containers/UploadImage.js
+++ src/containers/UploadImage.js
@@ -8,10 +8,16 @@ import { Icon } from 'antd';
 
 class UploadImage extends React.Component {
 
+  state = {
+    fileList: [],
+  };
+
+  handleChange = ({ fileList }) => this.setState({ fileList })
+
   render() {
 
+    const { fileList } = this.state;
     const { Title } = Typography;
-
     return (
       <Grid style={{ paddingTop: 100, backgroundColor: '#f1f1f1', height: '100vh', }}>
         <Row style={{ textAlign: 'center' }} >
@@ -19,18 +25,29 @@ class UploadImage extends React.Component {
             <Col span={12} style={{ textAlign: 'center', padding: '20px', height: '50vh', backgroundColor: '#fff', display: 'flex', borderTopLeftRadius: 5, borderBottomLeftRadius: 5, }}>
               <Grid style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', flex: 100, }}>
                 <Title style={{ color: '#1DA57A', fontSize: 16, textTransform: 'uppercase', }}>Upload Your Invoice</Title>
-                <Upload>
-                  <Button style={{ height: 110, width: 126, fontSize: 14, }}>
+                <Upload
+                  action="http://localhost:5000/invoice"
+                  fileList={fileList}
+                  name="invoice"
+                  onChange={this.handleChange}
+                >
+                  {fileList.length ? null : <Button style={{ height: 110, width: 126, fontSize: 14, }}>
                     <Icon type="upload" style={{ display: 'block', fontSize: 20, }} />
                     <Typography>Click to Upload</Typography>
                   </Button>
-                  <Title style={{ fontSize: 13, marginTop: 15, fontWeight: 400, color: '#9c9c9cd9' }}>Max File Size: 5MB</Title>
+                  }
+                  <Title style={{ fontSize: 13, marginTop: 15, fontWeight: 400, color: '#9c9c9cd9' }}>
+                    Max File Size: 5MB
+                  </Title>
                 </Upload>
               </Grid>
             </Col>
             <Col span={12} style={{ textAlign: 'center', padding: '20px', borderLeft: '1px solid #eee ', height: '50vh', backgroundColor: '#fff', borderTopRightRadius: 5, borderBottomRightRadius: 5, display: 'flex', flex: 100, alignItems: 'center', justifyContent: 'center', }}>
               <Grid>
-                <Title level={3} style={{ color: '#f1f1f1' }}>Preview Image</Title>
+                {fileList.length && fileList[0].response
+                  ? <img src={fileList[0].response.fileUrl} style={{ width: "100%" }} />
+                  : <Title level={3} style={{ color: '#f1f1f1' }}>Preview Image</Title>
+                }
               </Grid>
             </Col>
           </Col>
